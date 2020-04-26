b88a18654da944cce35d94c506e30fa54be6d341
Davi913 <rio.daviduarte@gmail.com>
2020-04-26 15:46:51 -0300

    :pushpin: Created project
    Only a simple scrim bot

--- src/main/java/application/Lauren.java
+++ src/main/java/application/Lauren.java
@@ -8,6 +8,9 @@ import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
+import java.io.*;
+import java.util.stream.Collectors;
+
 public class Lauren {
 
     public static JDA bot;
@@ -17,7 +20,20 @@ public class Lauren {
     public static void main(String[] args) throws Exception {
         logger = new LoggerDataSource();
         Logger.log("Lauren is now registering logs").save();
-        bot = new JDABuilder(AccountType.BOT).setToken("NzAyNTE4NTI2NzUzMjQzMTU2.XqBNhA.JxhSeEzwfbCUk2VzN8pfgtxwurQ").build();
+        File file = new File("token.json");
+        if (!file.exists()) {
+            if (!file.createNewFile()) return;
+            Logger.log("Coloque um token válido no bot.").save();
+            return;
+        }
+        BufferedReader reader = new BufferedReader(new FileReader(file.getAbsolutePath()));
+        String botToken = reader.lines().collect(Collectors.toList()).get(0);
+        reader.close();
+        if (botToken == null) {
+            Logger.log("Coloque um token válido no arquivo 'token.json'").save();
+            return;
+        }
+        bot = new JDABuilder(AccountType.BOT).setToken(botToken).build();
         bot.setAutoReconnect(true);
         new ListenersStartup(bot, "events", "MemberEvents");
         new CommandStartup(bot, "commands", "ServerInfoCommand", "ClearCommand", "PingCommand", "RegisterCommand", "InfoCommand");
