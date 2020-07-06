e97392eaf9be0a0b248756f1d22aee9972dbe2f1
Davi913 <rio.daviduarte@gmail.com>
2020-07-06 01:48:09 -0300

    :recycle: Added link blocker

--- src/main/java/com/yuhtin/lauren/events/ChatMessage.java
+++ src/main/java/com/yuhtin/lauren/events/ChatMessage.java
@@ -3,6 +3,8 @@ package com.yuhtin.lauren.events;
 import com.yuhtin.lauren.core.player.controller.PlayerDataController;
 import com.yuhtin.lauren.service.CommandCache;
 import com.yuhtin.lauren.utils.helper.LevenshteinCalculator;
+import com.yuhtin.lauren.utils.helper.Utilities;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -25,6 +27,12 @@ public class ChatMessage extends ListenerAdapter {
                     }
                 }
             }
+        }
+
+        if (event.getMessage().getContentRaw().contains("https://") && Utilities.isPermission(event.getMember(), event.getChannel(), Permission.MESSAGE_MANAGE)) {
+            event.getChannel().sendMessage("<:chorano:726207542413230142> Poxa, não divulga aqui amigo, temos nosso sistema de parceria, fale com o <@272879983326658570> no privado.").queue();
+            event.getMessage().delete().queue();
+            return;
         }
 
         PlayerDataController.get(event.getMember().getIdLong()).gainXP(3).updateLevel().save();
