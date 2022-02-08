ea7fce8369053b6c9f831391461629fe28f51417
Yuhtin <rio.daviduarte@gmail.com>
2022-02-08 02:48:06 -0300

    fix: exp command

--- src/main/java/com/yuhtin/lauren/commands/impl/admin/ClearCommand.java
+++ src/main/java/com/yuhtin/lauren/commands/impl/admin/ClearCommand.java
@@ -5,46 +5,43 @@ import com.yuhtin.lauren.commands.Command;
 import com.yuhtin.lauren.core.logger.Logger;
 import com.yuhtin.lauren.commands.CommandData;
 import com.yuhtin.lauren.utils.helper.UserUtil;
+import lombok.val;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageHistory;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.CommandInteraction;
 import net.dv8tion.jda.api.requests.restaction.MessageAction;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @CommandData(
-        name = "clear",
+        name = "clearchat",
         type = CommandData.CommandType.ADMIN,
         description = "Limpar algumas mensagens do canal atual",
-        alias = {"clearchat", "cc"})
+        args = {
+                "<!quantity>-Quantidade de mensagens para apagar",
+                "[@user]-Mencione um usuário para apagar as mensagens só dele"
+        }
+)
 public class ClearCommand implements Command {
 
     @Inject private Logger logger;
 
     @Override
-    public void execute(CommandEvent event) {
-
-        if (!UserUtil.hasPermission(event.getMember(), event.getMessage(), Permission.MESSAGE_MANAGE, true)) return;
-
-        String[] args = event.getMessage().getContentRaw().split(" ");
-        if (args.length < 2) {
-            MessageAction message = event.getChannel().sendMessage("❌ Utilize $clear <numero> (mention).");
-            message.queue(m -> m.delete().queueAfter(5, TimeUnit.SECONDS));
-            return;
-        }
-
-        int purge = Integer.parseInt(args[1]);
-        if (purge > 100) {
-            MessageAction message = event.getChannel().sendMessage("❌ O limite é de 100 mensagens por vez.");
-            message.queue(m -> m.delete().queueAfter(5, TimeUnit.SECONDS));
-            return;
-        }
+    public void execute(CommandInteraction event, InteractionHook hook) throws Exception {
+        if (event.getMember() == null || !UserUtil.hasPermission(event.getMember(), hook, Permission.MESSAGE_MANAGE)) return;
 
         long id = 0L;
+        val userOption = event.getOption("user");
+        if (userOption != null) id = userOption.getAsMember().getIdLong();
 
-        if (args.length > 2)
-            id = event.getMessage().getMentionedMembers().get(0).getIdLong();
+        val purge = (int) event.getOption("quantity").getAsDouble();
+        if (purge > 100) {
+            hook.sendMessage("❌ O limite é de 100 mensagens por vez.").queue();
+            return;
+        }
 
         MessageHistory messageHistory = new MessageHistory(event.getChannel());
         List<Message> messages;
@@ -54,15 +51,19 @@ public class ClearCommand implements Command {
         for (Message message : messages) {
             if (message == null || (id != 0L && message.getAuthor().getIdLong() != id)) continue;
 
-            event.getChannel().deleteMessageById(message.getId()).queue();
+            logger.info(String.format("User %s (%s) cleared message from %s (%s): %s",
+                    event.getUser().getAsTag(),
+                    event.getUser().getId(),
+                    message.getAuthor().getAsTag(),
+                    message.getAuthor().getId(),
+                    message.getContentDisplay()
+            ));
+
+            message.delete().queue();
             ++cleared;
         }
 
-        event.getChannel().sendMessage("<:online:703089222021808170> Foram apagadas **" + cleared + "** mensagens deste canal.")
-                .queue(m -> m.delete().queueAfter(5, TimeUnit.SECONDS));
-
-        this.logger.info("The user " + event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator()
-                + " cleared " + cleared + " messages from channel #" + event.getChannel().getName()
-                + " (" + event.getChannel().getId() + ")");
+        hook.sendMessage("<:online:703089222021808170> Foram apagadas **" + cleared + "** mensagens deste canal.").queue();
     }
+
 }
---
+++ src/main/java/com/yuhtin/lauren/commands/impl/admin/ExpCommand.java
@@ -0,0 +1,53 @@
+package com.yuhtin.lauren.commands.impl.admin;
+
+import com.google.inject.Inject;
+import com.yuhtin.lauren.commands.Command;
+import com.yuhtin.lauren.commands.CommandData;
+import com.yuhtin.lauren.core.player.controller.PlayerController;
+import com.yuhtin.lauren.utils.helper.UserUtil;
+import lombok.val;
+import lombok.var;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.CommandInteraction;
+
+@CommandData(
+        name = "exp",
+        type = CommandData.CommandType.CONFIG,
+        description = "Alterar o exp de um jogador",
+        args = {
+                "<option>-Use add, remove ou set.",
+                "<@player>-Jogador que deseja fazer a alteração.",
+                "<!quantity>-Quantidade de exp que deseja adicionar ou remover."
+        }
+)
+public class ExpCommand implements Command {
+
+    @Inject private PlayerController playerController;
+
+    @Override
+    public void execute(CommandInteraction event, InteractionHook hook) throws Exception {
+        if (event.getMember() == null
+                || !UserUtil.hasPermission(event.getMember(), hook, Permission.ADMINISTRATOR))
+            return;
+
+        val option = event.getOption("option").getAsString();
+        val player = event.getOption("player").getAsUser();
+        var quantity = (int) event.getOption("quantity").getAsDouble();
+        if (option.equalsIgnoreCase("remove")) quantity *= -1;
+
+        val data = playerController.get(player.getIdLong());
+        if (!option.equalsIgnoreCase("set")) quantity += data.getRankedPoints();
+
+        data.setRankedPoints(quantity);
+        data.updateRank();
+
+        if (quantity <= 0) quantity *= -1;
+        val optionUsed = option.equalsIgnoreCase("set") ? "setou" : option.equalsIgnoreCase("remove") ? "removeu" : "adicionou";
+
+        hook.sendMessage("<:felizpakas:742373250037710918> " +
+                        "Você " + optionUsed + " **" + quantity + "** exp ao jogador " + player.getAsTag())
+                .queue();
+    }
+
+}
