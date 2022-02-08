96f6682921fc1795b16ed5fc7638611d17e0ef72
Yuhtin <rio.daviduarte@gmail.com>
2022-02-08 02:30:49 -0300

    fix all draw commands

--- src/main/java/com/yuhtin/lauren/commands/impl/admin/draw/RerollCommand.java
+++ src/main/java/com/yuhtin/lauren/commands/impl/admin/draw/RerollCommand.java
@@ -5,6 +5,8 @@ import com.yuhtin.lauren.commands.CommandData;
 import com.yuhtin.lauren.core.draw.controller.DrawController;
 import com.yuhtin.lauren.utils.helper.UserUtil;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.CommandInteraction;
 
 @CommandData(
         name = "reroll",
@@ -14,15 +16,16 @@ import net.dv8tion.jda.api.Permission;
 public class RerollCommand implements Command {
 
     @Override
-    public void execute(CommandEvent event) {
-        if (!UserUtil.hasPermission(event.getMember(), event.getMessage(), Permission.ADMINISTRATOR, true)) return;
+    public void execute(CommandInteraction event, InteractionHook hook) throws Exception {
+        if (event.getMember() == null || !UserUtil.hasPermission(event.getMember(), hook, Permission.ADMINISTRATOR)) return;
 
         if (DrawController.get() == null || !DrawController.get().finished) {
-            event.getMessage().delete().queue();
+            hook.setEphemeral(true).sendMessage(":x: Nenhum sorteio ativo no momento.").queue();
             return;
         }
 
-        event.getChannel().sendMessage("♻️ Eu realmente não sei o que aconteceu, mas, como mandaram, sorteando um novo vencedor").queue();
+        hook.sendMessage("♻️ Eu realmente não sei o que aconteceu, mas, como mandaram, sorteando um novo vencedor").queue();
         DrawController.get().finish();
     }
+
 }
