65069ee5cc7edbaf27a953df6a3b875cf8f0104a
Yuhtin <rio.daviduarte@gmail.com>
2022-02-15 02:59:11 -0300

    fix: date util

--- src/main/java/com/yuhtin/lauren/utils/DateUtil.java
+++ src/main/java/com/yuhtin/lauren/utils/DateUtil.java
@@ -3,8 +3,6 @@ package com.yuhtin.lauren.utils;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * @author Yuhtin
  * Github: https://github.com/Yuhtin
@@ -13,7 +11,7 @@ import java.util.concurrent.TimeUnit;
 public final class DateUtil {
 
     public static String format(long time) {
-        return "<t:" + TimeUnit.MILLISECONDS.toSeconds(time) + ":R>";
+        return "<t:" + time + ":R>";
     }
 
 }
