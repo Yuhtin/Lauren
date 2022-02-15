2bb97c0ac82a47f3831f32077823e4fdf6161102
Yuhtin <rio.daviduarte@gmail.com>
2022-02-15 02:31:56 -0300

    fix: interaction failing

--- src/main/java/com/yuhtin/lauren/commands/CommandCatcher.java
+++ src/main/java/com/yuhtin/lauren/commands/CommandCatcher.java
@@ -22,23 +22,22 @@ public final class CommandCatcher extends ListenerAdapter {
 
     @Override
     public void onSlashCommand(@NotNull SlashCommandEvent event) {
-        val commands = commandMap.getCommands();
-
-        var name = event.getName();
-        val subcommandName = event.getSubcommandName();
-        if (subcommandName != null) {
-            name += "." + subcommandName;
-        }
-
-        val logger = Startup.getLauren().getLogger();
-        val command = commands.getOrDefault(name, null);
-        if (command == null) {
-            logger.info("Não encontrei o comando " + name);
-            return;
-        }
-
-        String finalName = name;
         event.deferReply().queue(hook -> {
+            val commands = commandMap.getCommands();
+
+            var name = event.getName();
+            val subcommandName = event.getSubcommandName();
+            if (subcommandName != null) {
+                name += "." + subcommandName;
+            }
+
+            val logger = Startup.getLauren().getLogger();
+            val command = commands.getOrDefault(name, null);
+            if (command == null) {
+                logger.info("Não encontrei o comando " + name);
+                return;
+            }
+            
             try {
                 command.execute(event, hook);
             } catch (Exception exception) {
